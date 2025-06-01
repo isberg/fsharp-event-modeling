df7d83f083bb4bd53c712289ec3b185b4a5c26f0
Jonas Isberg <jonas.d.isberg@gmail.com>
2025-06-01 18:50:50 +0200

    Add automation support to Service

--- Service.fs
+++ Service.fs
@@ -22,9 +22,9 @@ type Service<'State,'Command,'Event>
 
 open Serilog
     
-let createService<'State,'Command,'Event when 'Event :> TypeShape.UnionContract.IUnionContract>
-    : CommandPattern.Decider<'State,'Command,'Event> -> string -> Service<'State,'Command,'Event> 
-    = fun decider categoryName ->
+let createService<'View,'State,'Command,'Event when 'Event :> TypeShape.UnionContract.IUnionContract>
+    : CommandPattern.Decider<'State,'Command,'Event> -> string -> AutomationPattern.Automation<'View,'State,'Event,'Command> option -> Service<'State,'Command,'Event> 
+    = fun decider categoryName automation ->
         let store : Equinox.MemoryStore.VolatileStore<obj> = Equinox.MemoryStore.VolatileStore()
         let log = LoggerConfiguration().WriteTo.Console().CreateLogger()
         let logEvents sn (events: FsCodec.ITimelineEvent<_>[]) =
@@ -45,11 +45,29 @@ let createService<'State,'Command,'Event when 'Event :> TypeShape.UnionContract.
             | events -> events
         let load : FsCodec.StreamName -> 'Event list = loadSafe >> Array.map codec.Decode >> Array.choose (function | ValueSome v -> Some v | ValueNone -> None) >> Array.toList
            
-        Service
-            ( FsCodec.StreamId.gen id >> Equinox.Decider.forStream log cat
-            , fun cmd state -> decider.decide cmd state |> List.toArray
-            , subscribe
-            , load
-            ) 
+        let service = 
+            Service
+                ( FsCodec.StreamId.gen id >> Equinox.Decider.forStream log cat
+                , fun cmd state -> decider.decide cmd state |> List.toArray
+                , subscribe
+                , load
+                )
+        
+        match automation with
+        | Some automation' ->
+            let _ : System.IDisposable = service.Subscribe (
+                fun streamName events -> 
+                    let events' = service.Load streamName
+                    let view = ViewPattern.hydrate automation'.projection events'
+                    let struct (name, streamId) =  FsCodec.StreamName.split streamName
+                    match automation'.trigger view with
+                    | Some cmd -> Async.RunSynchronously <| service.Execute (streamId.ToString()) cmd 
+                    | None -> ()
+                    ()
+            )
+            ()
+        | None -> ()
+
+        service
 
 
