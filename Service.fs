d24ff71c4c72bcfdc2f0c75bc2b60fec9acbab32
isberg <jonas.d.isberg@gmail.com>
2025-06-19 12:17:29 +0200

    Fix EventStoreDB subscription and bump version

--- Service.fs
+++ Service.fs
@@ -163,20 +163,21 @@ let createService<'View,'State,'Command,'Event,'TState,'TCommand,'TEvent,'TView
                 |> ValueOption.toOption
 
             let subscribe callback =
+                let eventAppeared _ ev _ =
+                    match FsCodec.StreamName.tryParse ev.Event.EventStreamId with
+                    | ValueSome sn when FsCodec.StreamName.category sn = categoryName ->
+                        match decode ev with
+                        | Some e -> callback sn [ e ]
+                        | None -> ()
+                    | _ -> ()
+                    System.Threading.Tasks.Task.CompletedTask
                 let filter =
                     EventStore.Client.SubscriptionFilterOptions(
                         EventStore.Client.StreamFilter.Prefix categoryName)
                 let sub =
                     client.SubscribeToAllAsync(
                         EventStore.Client.Position.Start,
-                        fun _ ev _ ->
-                            match FsCodec.StreamName.tryParse ev.Event.EventStreamId with
-                            | ValueSome sn when FsCodec.StreamName.category sn = categoryName ->
-                                match decode ev with
-                                | Some e -> callback sn [ e ]
-                                | None -> ()
-                            | _ -> ()
-                            System.Threading.Tasks.Task.CompletedTask,
+                        eventAppeared,
                         false,
                         filter)
                 { new System.IDisposable with member _.Dispose() = sub.Result.Dispose() }
